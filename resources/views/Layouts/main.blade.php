3150dfec179c5374bec24a28efb8352f85930219
madhavraj1997 <neupanemadhavraj1997@gmail.com>
2022-10-12 17:05:55 +0545

    portfolio first commit

---
+++ resources/views/Layouts/main.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&family=Roboto&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="{{asset('Frontend/CSS/style.css')}}" rel="stylesheet">
+    <title>Portfolio</title>
+</head>
+
+<body>
+
+    <div class="hero">
+        <nav>
+            <img src="./img/img1.webp" class="logo" />
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+    
+        </nav>
+
+        <div class="content">
+            <span class="title">Web Developer</span>
+            <h1>Hello, I’m <span>MadhavRaj</span></h1>
+            <p>
+                I am currently doing internship from eSewa Travel and Tours as web developer, and pursuing Bsc.
+                And completed graduation in 2022 with computer science and information technology. I am passionate toward backend and some
+                hands-on HTML, CSS and javascript.
+            </p>
+            <a href="#" class="btn">Download CV</a>
+        </div>
+        <div class="content">
+            <span class="title">Web Developer</span>
+        </div>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
